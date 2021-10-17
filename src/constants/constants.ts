c6d4d08f93a98b7e6277d4734fa5ca27ab6b518d
Vafa Nobakht <vafa.nobakht23@gmail.com>
2021-10-18 02:07:06 +0330

    Filter bu month-week-day and change some part of project and complete them to do best

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,18 +1,18 @@
 import { TodoObj } from '../interface/model';
 
 const object: TodoObj = {
-    id: '',
-    title: '',
-    status: '',
-    isComplete: false,
-    date: '',
-    time: '',
+  id: '',
+  title: '',
+  status: '',
+  isComplete: false,
+  date: '',
+  time: '',
 };
 
 const status = {
-    done: 'Done',
-    progress: 'Progress',
-    pause: 'Paused',
+  done: 'Done',
+  progress: 'Progress',
+  pause: 'Paused',
 };
 
 export { status, object };
--- src/helper/function.ts
+++ src/helper/function.ts
@@ -1,14 +1,80 @@
 import { TodoObj } from '../interface/model';
 
 const calculateDate = (todo: TodoObj): string => {
-    const date = new Date(todo.date);
-    const today = date.toLocaleDateString('en-GB', {
-        day: 'numeric',
-        month: 'long',
-        year: 'numeric',
-    });
-    return today;
+  const date = new Date(todo.date);
+  const today = date.toLocaleDateString('en-GB', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  });
+  return today;
 };
 
+const dateType = (): string => {
+  const date = new Date();
+  const today = date.toLocaleDateString('en-GB', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  });
+  return today;
+};
 
-export { calculateDate }
+const filterMonth = (todos: TodoObj[]): TodoObj[] => {
+  const today = dateType();
+  const array = today.split(' ');
+  const month = array[1];
+  const year = array[2];
+  const newMonthArray = todos.filter((todo) =>
+    todo.date.split(' ')[1] === month && todo.date.split(' ')[2] === year
+      ? todo
+      : null,
+  );
+  return sort(newMonthArray);
+};
+
+const filterWeek = (todos: TodoObj[]): TodoObj[] => {
+  const newTodoArray = sort(todos);
+  const today = dateType();
+  const array = today.split(' ');
+  const day = array[0];
+  const month = array[1];
+  const year = array[2];
+  const newWeekArray = newTodoArray.filter((todo) =>
+    parseInt(todo.date.split(' ')[0]) >= parseInt(day) &&
+    parseInt(todo.date.split(' ')[0]) < parseInt(day) + 7 &&
+    todo.date.split(' ')[1] === month &&
+    todo.date.split(' ')[2] === year
+      ? todo
+      : null,
+  );
+  return newWeekArray;
+};
+
+const filterToday = (todos: TodoObj[]): TodoObj[] => {
+  const today = dateType();
+  const array = today.split(' ');
+  const day = array[0];
+  const month = array[1];
+  const year = array[2];
+  const newDayArray = todos.filter((todo) =>
+    todo.date.split(' ')[0] === day &&
+    todo.date.split(' ')[1] === month &&
+    todo.date.split(' ')[2] === year
+      ? todo
+      : null,
+  );
+  return sort(newDayArray);
+};
+
+const sort = (todoArray: TodoObj[]) => {
+  todoArray.sort((a, b) => {
+    return (
+      new Date(a.date + ' ' + a.time).getTime() -
+      new Date(b.date + ' ' + b.time).getTime()
+    );
+  });
+  return todoArray;
+};
+
+export { calculateDate, filterMonth, filterToday, filterWeek };
